b67f198161a92d6ffea8234e174404e81b976d98
lukeisun <lukeisun57@gmail.com>
2023-10-04 23:17:04 -0400

    base functionality down, can prompt ai with context :)

--- main.go
+++ main.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"bytes"
-	// "context"
-	"encoding/json"
+	"context"
 	"fmt"
-	"github.com/a-h/templ"
+
+	// "github.com/a-h/templ"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
-	// "os"
-	"github.com/labstack/echo/v4"
 	"strings"
+
+	"github.com/labstack/echo/v4"
 )
 
-// take in string
-//
-//	run through ai
-//
-// return html
-func main() {
-	e := echo.New()
+func get_response(message string, currentContext string) string {
 	request_url := "http://localhost:11434/api/generate"
-	jsonStr := []byte(`{"model":"mistral", "prompt":"Why is the sky blue?"}`)
-	resp, err := http.Post(request_url, "application/json", bytes.NewBuffer(jsonStr))
+	var jsonBytes []byte
+	msg := strings.ReplaceAll(message, "\"", "")
+	if currentContext == "" {
+		jsonBytes = []byte(`{"model":"mistral", "prompt":"` + msg + `"}`)
+	} else {
+		jsonBytes = []byte(`{"model":"mistral", "prompt":"` + msg + `", "context":[` + currentContext + `]}`)
+	}
+
+	fmt.Println(string(jsonBytes))
+	resp, err := http.Post(request_url, "application/json", bytes.NewBuffer(jsonBytes))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,27 +35,50 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	string_arr := strings.Split(string(body), "\n")
-	arr := make([]map[string]interface{}, 0)
-	for _, line := range string_arr {
+	body_arr := strings.Split(string(body), "\n")
+	json_arr := make([]map[string]interface{}, 0)
+	for _, line := range body_arr {
 		var data map[string]interface{}
 		json.Unmarshal([]byte(line), &data)
-		arr = append(arr, data)
+		json_arr = append(json_arr, data)
 	}
-	resp_arr := make([]string, 0)
-	for _, line := range arr {
+	ret_arr := make([]string, 0)
+	var chatContext []int
+	for _, line := range json_arr {
 		s, ok := line["response"].(string)
 		if ok {
-			resp_arr = append(resp_arr, s)
+			ret_arr = append(ret_arr, s)
+			continue
+		}
+		contextValue, ok := line["context"].([]interface{})
+		if ok {
+			for _, val := range contextValue {
+				chatContext = append(chatContext, int(val.(float64)))
+			}
 		}
 	}
-	ret := strings.Join(resp_arr, "")
-	component := hello(ret)
-	http.Handle("/", templ.Handler(component))
-	http.ListenAndServe(":8080", nil)
-	// var data map[string]interface{}
-	// if err := json.Unmarshal(body, &data); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(data)
+	chatCtxString := strings.Trim(strings.Join(strings.Split(fmt.Sprint(chatContext), " "), ","), "[]")
+	var bytes bytes.Buffer
+	paragraph := strings.Join(ret_arr, "")
+	err = postReply(paragraph, chatCtxString).Render(context.Background(), &bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bytes.String()
+}
+func main() {
+	e := echo.New()
+	// keep context somehow
+	e.Static("/static", "static")
+	e.POST("/request", func(c echo.Context) error {
+		message := c.FormValue("entry")
+		currentContext := c.FormValue("context")
+		chatReply := get_response(message, currentContext)
+		return c.HTML(http.StatusOK, chatReply)
+	})
+	e.GET("/", func(c echo.Context) error {
+		return c.File("static/index.html")
+	})
+	fmt.Println("Server started at port 1323")
+	e.Logger.Fatal(e.Start(":1323"))
 }
